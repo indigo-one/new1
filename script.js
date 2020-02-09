d3484b78991b79b76aebbacefb1acf5ca14d355c
indigo-one <54807953+indigo-one@users.noreply.github.com>
2020-02-09 22:03:08 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+//0alert('wdewedwed');
+//console.log(2+2);
+//out.innerHTML = '<b>Перезаписал</b>';
+//out.innerText = '<b>Перезаписал</b>';
+//document.getElementById('out').innerHTML = 777;
+let p;
+p = document.getElementById('out');
+p = document.querySelector('#out');
+p.innerHTML = 5555;
+console.log(p);
+let c = 'Hello ';
+let d = 'world';
+console.log( c + d );
+let year = 2020;
+console.log(year - 1981)
+
+let MyYear = document.querySelector('#MyYear');
+MyYear.innerHTML = '<i>1981</i>';
